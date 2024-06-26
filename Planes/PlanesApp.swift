fd30cad3c582762ac195a7ef871dbb9041a67572
danimalbiscuits <daniel.geraghty7@gmail.com>
2024-06-26 19:29:14 +1200

    Initial Commit

---
+++ Planes/PlanesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanesApp.swift
+//  Planes
+//
+//  Created by Daniel Geraghty on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
