347f4250ef4a86dbdf6a1b1030964d2081059026
danimalbiscuits <daniel.geraghty7@gmail.com>
2024-06-26 21:08:05 +1200

    map added

---
+++ Planes/FlightDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FlightDetailView.swift
+//  Planes
+//
+//  Created by Daniel Geraghty on 26/06/2024.
+//
+
+import SwiftUI
+
+struct FlightDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlightDetailView()
+}
